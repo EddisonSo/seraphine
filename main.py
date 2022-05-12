95f39d4e500039e04e148fedbe16ae81257e99f1
Eddison So <eddisonso@gmail.com>
2022-05-12 12:36:07 -0400

    sentiment analysis

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import discord
 from random import randint
+from textblob import TextBlob
+#from time import sleep
 
 client = discord.Client()
 
-firstmove = []
+greet = []
+positive = []
+negative = []
 
-def get_first_move():
-    file = open("firstmove.txt")
-    global firstmove
-    firstmove = file.read().splitlines()
+def get_greet():
+    file = open("greet.txt")
+    global first_move
+    greet = file.read().splitlines()
     file.close()
 
 async def print_first_move(message):
-    global firstmove
-    await message.channel.send(firstmove[randint(0,len(firstmove)-1)])
+    global first_move
+    await message.channel.send(greet[randint(0,len(greet)-1)])
+
+def get_negative_message():
+    file = open("negative.txt")
+    global negative
+    negative = file.read().splitlines()
+    file.close()
+
+async def print_negative_message(message):
+    global negative
+    await message.channel.send(negative[randint(0,len(negative)-1)])
+
+def get_positive_message():
+    file = open("positive.txt")
+    global positive
+    positive = file.read().splitlines()
+    file.close()
+
+async def print_positive_message(message):
+    global positive
+    await message.channel.send(positive[randint(0,len(positive)-1)])
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    get_greet()
+    get_negative_message()
+    get_positive_message()
 
 @client.event
 async def on_message(message):
@@ -27,11 +54,10 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await print_first_move(message)
 
-    if message.content.lower() == "i love you seraphine":
-        await message.channel.send("I love you too <@" + str(message.author.id) + ">")
-
-    if message.content.lower() == "$kiss":
-        await message.channel.send("uwu not now <@" + str(message.author.id) + ">")
+    if "seraphine" in message.content and TextBlob(message.content).sentiment.polarity >= 0:
+        await print_positive_message(message)
+    else:
+        await print_negative_message(message)
 
 
 client.run('')
